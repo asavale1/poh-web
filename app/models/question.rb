463ee6d3a0a16d71d2ec6b5b87fc0db0cbd04564
Ameya <asavale1@yahoo.com>
2015-09-05 11:49:54 -0700

    model validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,11 @@
+# => question 	: string(255)
+# => yes_count 	: integer
+# => no_count	: integer
 class Question < ActiveRecord::Base
+	validates :question, presence: true
+
+	after_initialize do
+		self.yes_count ||= 0
+		self.no_count ||= 0
+	end
 end
