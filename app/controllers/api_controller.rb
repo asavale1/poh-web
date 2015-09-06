6861a8d661f6b579ea422e87d6966d16243a6158
Ameya <asavale1@yahoo.com>
2015-09-06 03:44:00 -0700

    check answer

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,16 @@
 class ApiController < ApplicationController
 	skip_before_filter :verify_authenticity_token, :only => [:post_answer, :get_question]
+	
 	def post_answer
 		puts params
+		q = Question.find(params[:question_id])
+		
+		if(params[:answer] =~ /yes/)
+			puts "Answer is yes"
+		elsif params[:answer] =~ /no/
+			puts "Answer is no"
+		end
+
 		render :json => { :status => "success" }
 	end
 
